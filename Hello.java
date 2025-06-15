6acf3eeea28d10986ac8dd85942bbd04a1874be5
Parag H <curiousph93@gmail.com>
2025-06-15 22:24:03 +0530

    HelloWorld commit

---
+++ Hello.java
@@ -0,0 +1,7 @@
+//Heres a simple Java "Hello World" program. It defines a class with a `main` method that prints "Hello, World!" to the console.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
