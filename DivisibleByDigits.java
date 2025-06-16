bda35c05924b583ea15df0f1839559f50af6c50a
kulkarnishweta3 <113980129+kulkarnishweta3@users.noreply.github.com>
2025-06-16 22:36:40 +0530

    Update DivisibleByDigits.java

--- DivisibleByDigits.java
+++ DivisibleByDigits.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class DivisibleByDigits {
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the user
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
