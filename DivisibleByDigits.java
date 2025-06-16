22335faa72976083e08badd66fc69a42d6fd035c
Parag H <curiousph93@gmail.com>
2025-06-16 22:22:58 +0530

    Day 2 commit

---
+++ DivisibleByDigits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DivisibleByDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        boolean found = false;
+        System.out.print("Divisible by: ");
+        for (int i = 1; i <= 9; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.print("None");
+        }
+        System.out.println();
+    }
+}
