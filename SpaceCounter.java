3a305b0987ab0f510c67dc897e5a755574fdc134
kulkarnishweta3 <113980129+kulkarnishweta3@users.noreply.github.com>
2025-06-22 20:29:02 +0530

    Add files via upload
    Commit day #7

---
+++ SpaceCounter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SpaceCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        int spaceCount = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ' ') {
+                spaceCount++;
+            }
+        }
+
+        System.out.println("Number of spaces: " + spaceCount);
+    }
+}
