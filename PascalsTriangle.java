6c5812805f9669474c3b638dd8a74995a142ab79
kulkarnishweta3 <113980129+kulkarnishweta3@users.noreply.github.com>
2025-07-14 21:35:26 +0530

    Add files via upload
    Commit code #14-July-25

---
+++ PascalsTriangle.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PascalsTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;
+        List<List<Integer>> result = generate(numRows);
+        for (List<Integer> row : result) {
+            System.out.println(row);
+        }
+    }
+}
