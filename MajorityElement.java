a2cc59b42af9b772a39612a03f4c2dde35045200
kulkarnishweta3 <113980129+kulkarnishweta3@users.noreply.github.com>
2025-07-23 22:29:27 +0530

    Add files via upload
    commit code #24-07-25

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,4 +1,5 @@
 public class MajorityElement {
+    // This method finds the majority element in an array using the Boyer-Moore Voting Algorithm.
     public static int majorityElement(int[] nums) {
         int count = 0, candidate = 0;
         for (int num : nums) {
